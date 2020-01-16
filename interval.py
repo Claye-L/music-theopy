257d1c344c083b63b089c3d05fe3696c15abca18
Claye_L <claye@ixcys.fr>
2020-01-16 14:23:12 +0100

    refactor into files and methods

---
+++ interval.py
@@ -0,0 +1,45 @@
+from enum import Enum
+
+'''represents an interval either by major scale name or pitch value'''
+class Interval(Enum):
+	Unison = 0
+	MinorSecond = 1
+	Second = 2
+	MinorThird = 3
+	MajorThird = 4
+	Fourth = 5
+	Tritone = 6
+	SharpFour = 6
+	FlatFive = 6
+	Fifth = 7
+	MinorSixth = 8
+	MajorSixth = 9
+	MinorSeventh = 10
+	MajorSeventh = 11
+
+	def noteOffsetForInterval(self):
+		value = self.value
+		if value == 0:
+			return 0
+		elif value in [1,2]:
+			return 2
+		elif value in [3,4]:
+			return 4
+		elif value in [5,6]:
+			return 5
+		elif value == 7:
+			return 7
+		elif value in [8,9]:
+			return 9
+		else:
+			return 11
+	'''returns how to shift the name of a note for an interval according to major scale rules'''
+	def intervalShift(self):
+		interval = self
+		if interval.value in [0,2,4,5,7,9,11]:
+			return (self.noteOffsetForInterval(),0)
+		elif interval.value in [1,3,6,8,10]:
+			return (self.noteOffsetForInterval(),-1)
+			
+			
+print(Interval.MinorThird.intervalShift())
